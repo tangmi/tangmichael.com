6b4808241c2d9de2a3856c4e0924a8b47c68c420
Michael Tang <tangmi@uw.edu>
2014-05-16 17:55:37 -0700

    add js

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+$(window).resize(function() {
+	console.log($(window).height());
+	$('.top').height($(window).height());
+});
+
+$(window).resize();
+
+// make a function that runs some `fn` at quickest, at some
+// interval `time` (in ms) 
+var makeLimiter = function(fn, time) {
+	var isRunning = false;
+	return function(ctx, args) {
+		if (!isRunning) {
+			fn.apply(ctx, args);
+			isRunning = true;
+			setTimeout(function() {
+				isRunning = false;
+			}, time);
+		}
+	};
+};
+
+
+function print(message) {
+	console.log('action', message);
+}
+
+var printLimiter = makeLimiter(print, 500);
+$(window).keypress(function(e) {
+	console.log(e.key);
+	printLimiter(this, [e.key]);
+});
